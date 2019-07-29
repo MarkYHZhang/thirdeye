823eeb02903d8f1997239837240a676014c037ca
MarkYHZhang <markzhang.inbox@gmail.com>
2019-07-28 23:25:48 -0700

    added displaying client file

---
+++ client/display.py
@@ -0,0 +1,35 @@
+import cv2
+import socket
+import struct
+import pickle
+
+class Display():
+
+    def __init__(self,):
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_socket.connect(('localhost', 3889))
+        self.view()
+
+    def view(self):
+        data = b""
+        payload_size = struct.calcsize(">L")
+        while True:
+            while len(data) < payload_size:
+                print("Recv: {}".format(len(data)))
+                data += self.client_socket.recv(4096)
+
+            print("Done Recv: {}".format(len(data)))
+            packed_msg_size = data[:payload_size]
+            data = data[payload_size:]
+            msg_size = struct.unpack(">L", packed_msg_size)[0]
+            print("msg_size: {}".format(msg_size))
+            while len(data) < msg_size:
+                data += self.client_socket.recv(4096)
+            frame_data = data[:msg_size]
+            data = data[msg_size:]
+            frame = pickle.loads(frame_data, fix_imports=True, encoding="bytes")
+            frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
+            cv2.imshow('ImageWindow', frame)
+            cv2.waitKey(1)
+
+Display()
