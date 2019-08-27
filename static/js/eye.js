1469a406e2833010a898d156f05fb32afdf8dde8
MarkYHZhang <markzhang.inbox@gmail.com>
2019-08-27 13:55:25 -0400

    minor changes

--- static/js/eye.js
+++ static/js/eye.js
@@ -1,34 +1,33 @@
 var ws;
-$(document).ready(function () {
-    let webcam = document.querySelector("#webcam");
-    let canvas = document.querySelector("#canvas");
-    let ctx = canvas.getContext('2d');
+let webcam = document.querySelector("#webcam");
+let canvas = document.querySelector("#canvas");
+let ctx = canvas.getContext('2d');
 
-    var localMediaStream = null;
+var localMediaStream = null;
 
-    ws = new WebSocket("ws://"+window.location.hostname+":8889");
+ws = new WebSocket("ws://"+window.location.hostname+":8889");
 
-    function see() {
-        if (!localMediaStream) {
-            return;``
-        }
-
-        ctx.drawImage(webcam, 0, 0, webcam.videoWidth, webcam.videoHeight, 0, 0, 640, 480);
-
-        let dataURL = canvas.toDataURL('image/jpeg');
-        var cur_path = window.location.href.split('/');
-        var channel_str = cur_path[cur_path.length-1];
-        // console.log(channel_str);
-        // packet = JSON.stringify({"channel": "main", "frame":dataURL});
-        packet = JSON.stringify({"channel": channel_str, "frame":dataURL});
-        ws.send(packet);
+function see() {
+    if (!localMediaStream) {
+        return;``
     }
-    var constraints = {
-        video: {
-            width: { min: 640 },
-            height: { min: 480 }
-        }
-    };
+
+    ctx.drawImage(webcam, 0, 0, webcam.videoWidth, webcam.videoHeight, 0, 0, 640, 480);
+
+    let dataURL = canvas.toDataURL('image/jpeg');
+    var cur_path = window.location.href.split('/');
+    var channel_str = cur_path[cur_path.length-1];
+    // console.log(channel_str);
+    // packet = JSON.stringify({"channel": "main", "frame":dataURL});
+    packet = JSON.stringify({"channel": channel_str, "frame":dataURL});
+    ws.send(packet);
+}
+var constraints = {
+    video: {
+        width: { min: 640 },
+        height: { min: 480 }
+    }
+};
 
     navigator.mediaDevices.getUserMedia(constraints).then(function (stream) {
         webcam.srcObject = stream;
@@ -37,4 +36,3 @@ $(document).ready(function () {
     }).catch(function (error) {
         console.log(error);
     });
-});
