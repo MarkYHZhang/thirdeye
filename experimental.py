939e0c28f65dbe82271ed8dee2c0bbefb5d4ba1e
MarkYHZhang <markzhang.inbox@gmail.com>
2019-08-28 14:35:54 -0700

    disconnected 'eye' sessions auto removal

--- experimental.py
+++ experimental.py
@@ -1,10 +1,12 @@
 import asyncio
 import websockets
+from websockets.exceptions import ConnectionClosedError
 from processor import Processor
 from flask import Flask, render_template, request, Response
 from queue import LifoQueue
 import threading
 import json
+import time
 
 class Brain():
 
@@ -12,21 +14,35 @@ class Brain():
         self.channels = {}
         self.processors = {}
         self.running = {}
+        self.last_visit = {}
+
+    def clean_disconnected_clients(self):
+        t = dict(self.last_visit.items())
+        for channel, last_visit in t:
+            if time.time() - last_visit > 5:
+                del self.last_visit[channel]
+                del self.running[channel]
+                del self.channels[channel]
+                del self.processors[channel]
 
     async def consumer_handler(self, websocket, path):
-        async for message in websocket:
-            data = json.loads(message)
-            channel = data['channel']
-            if channel in self.running and self.running[channel][0]:
-                continue
-            if channel not in self.channels:
-                self.channels[channel] = LifoQueue(-1)
-                self.processors[channel] = Processor()
-                self.running[channel] = [True]
-            t = threading.Thread(target=self.processors[channel].process, args=(self.running[channel], self.channels[channel], data['frame']))
-            t.start()
-            t.join(0)
-            # self.processors[channel].process(self.running[channel], self.channels[channel], data['frame'])
+        try:
+            async for message in websocket:
+                data = json.loads(message)
+                channel = data['channel']
+                if channel in self.running and self.running[channel][0]:
+                    continue
+                if channel not in self.channels:
+                    self.channels[channel] = LifoQueue(-1)
+                    self.processors[channel] = Processor()
+                    self.running[channel] = [True]
+                    self.last_visit[channel] = time.time()
+                t = threading.Thread(target=self.processors[channel].process, args=(self.running[channel], self.channels[channel], data['frame']))
+                t.start()
+                t.join()
+        except ConnectionClosedError:
+            print('Connection closed for an eye client')
+            self.clean_disconnected_clients()
 
 
 brain = Brain()
@@ -43,12 +59,6 @@ log = logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-
-
-# @app.route('/')
-# def index():
-#     return render_template('index.html')
-
 
 @app.route('/eye/<channel>')
 def eye(channel):
